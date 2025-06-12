df734fc740363647d02c7eeccf08d57bf436de1c
jihan datuslan <jihandatuslanibrahim@gmail.com>
2025-06-12 11:48:29 +0700

    Implementasi service registry dengan spring cloud

---
+++ src/test/java/com/tugas_akhir/registry_service/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugas_akhir.registry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
